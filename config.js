d0beb88816a93566a7fc10709f30ca141b7dbaba
Joona Pääkkönen <joona@giosg.com>
2015-06-08 22:12:14 +0300

    Added config and made authentication optional for now

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // GITHUB_TOKEN: '', // add if you don't want to run into rate limits
+    GITHUB_TARGET_REPO: 'paksu/duke', // user/repo for example from http://github.com/paksu/duke
+};
