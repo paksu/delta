cf8b2e214a5525eef417de9a021c99cfcca311aa
Joona Pääkkönen <joona@giosg.com>
2015-06-08 21:38:47 +0300

    Hacking stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var api = require('../lib/github');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  api.getRenderedPullRequests().then(function(data) {
+    res.render('index', { title: 'Express', data: data });
+  }, function(e) {
+    res.render('error', {
+      message: "Woah! Could not get pull requests because",
+      error: e
+    });
+  });
 });
 
 module.exports = router;
